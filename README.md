45e48d590c83b5e842243a2576814ffda5016b47
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-11-15 20:57:06 +0100

    add: Milestone 1, Bonus 2

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Stampare all'interno di una lista, un item per ogni todo.
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 - MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+-----------
+BONUS 2
+Trasformare ogni todo in un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no. Se done è true, mostrare il testo del todo sbarrato.
--- js/script.js
+++ js/script.js
@@ -2,10 +2,35 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      todo: [
+        {
+          text: "Fare lavatrice",
+          done: false,
+        },
+        {
+          text: "Prendere il latte",
+          done: false,
+        },
+        {
+          text: "Portare Fannie dalla veterinaria",
+          done: false,
+        },
+      ],
+    };
   },
   created() {
     console.log("Ciao");
   },
-  methods: {},
+  methods: {
+    checked: function (index) {
+      if (this.todo[index].done === false) {
+        this.todo[index].done = true;
+        console.log(this.todo[index].text, ":", "Fatto");
+      } else {
+        this.todo[index].done = false;
+        console.log(this.todo[index].text, ":", "Non fatto");
+      }
+    },
+  },
 }).mount("#app");
